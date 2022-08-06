cebb1497be9f3524286c57139fa2a05176a221ef
Anthony Shellman <atshellman@gmail.com>
2022-08-06 13:53:24 -0600

    Add import statements and comments for potential function sections

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,52 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+// ###########  Imports  ###########
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
-
+import {fetchData} from './apiCalls';
+import Booking from '.classes/Booking';
+import Customer from '.classes/Customer';
+import Reservation from '.classes/Reservation';
+import Room from '.classes/Room';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// ###########  Query Selectors ###########
+
+
+
+// ###########  Global Variables  ###########
+
+
+
+// ###########  Promises  ###########
+
+
+
+// ###########  Event Listeners  ###########
+
+
+
+// ###########  On-Load Functions  ###########
+
+
+
+
+// ###########  View Functions  ###########
+
+
+
+
+// ###########  Show/Hide View Functions  ###########
+
+
+
+
+// ###########  Search Functions  ###########
+
+
+
+
