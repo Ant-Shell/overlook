523d748d1a1dbae6cdc1a93805ca1b6e04dd3ef0
Anthony Shellman <atshellman@gmail.com>
2022-08-06 17:47:52 -0600

    Comment imports for now

--- src/scripts.js
+++ src/scripts.js
@@ -5,10 +5,11 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 import {fetchData} from './apiCalls';
-import Booking from '.classes/Booking';
-import Customer from '.classes/Customer';
-import Reservation from '.classes/Reservation';
-import Room from '.classes/Room';
+// import Booking from '.classes/Booking';
+// import Customer from '.classes/Customer';
+// import Reservation from '.classes/Reservation';
+// import Room from '.classes/Room';
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
