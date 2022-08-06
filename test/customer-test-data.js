69b71398403e526a088d402819cd63da76d326f7
Anthony Shellman <atshellman@gmail.com>
2022-08-06 13:32:24 -0600

    Add test data

---
+++ test/customer-test-data.js
@@ -0,0 +1,22 @@
+let customerData = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 9,
+    "name": "Faustino Quitzon"
+  },
+  {
+    "id": 13,
+    "name": "Christina Kulas"
+  },
+  {
+    "id": 20,
+    "name": "Keon Kirlin"
+  },
+  {
+    "id": 43,
+    "name": "Earline Hamill"
+  },
+]
