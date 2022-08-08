2a231b2e94c09271186eaf31caf46330226388bf
Anthony Shellman <atshellman@gmail.com>
2022-08-08 17:51:17 -0600

    Update data; add export

--- test/data/room-test-data.js
+++ test/data/room-test-data.js
@@ -1,43 +1,11 @@
 let roomData = [
   {
-    "number": 1,
-    "roomType": "residential suite",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 358.4
-  },
-  {
-    "number": 2,
-    "roomType": "suite",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 477.38
-  },
-  {
-    "number": 3,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "king",
-    "numBeds": 1,
-    "costPerNight": 491.14
-  },
-  {
     "number": 4,
     "roomType": "single room",
     "bidet": false,
     "bedSize": "queen",
     "numBeds": 1,
     "costPerNight": 429.44
-  },
-  {
-    "number": 5,
-    "roomType": "single room",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 2,
-    "costPerNight": 340.17
   },
   {
     "number": 6,
@@ -56,27 +24,45 @@ let roomData = [
     "costPerNight": 231.46
   },
   {
-    "number": 8,
-    "roomType": "junior suite",
-    "bidet": false,
-    "bedSize": "king",
-    "numBeds": 1,
-    "costPerNight": 261.26
-  },
-  {
-    "number": 9,
+    "number": 12,
     "roomType": "single room",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 200.39
-  },
-  {
-    "number": 10,
-    "roomType": "suite",
     "bidet": false,
     "bedSize": "twin",
+    "numBeds": 2,
+    "costPerNight": 172.09
+  },
+  {
+    "number": 15,
+    "roomType": "residential suite",
+    "bidet": false,
+    "bedSize": "full",
     "numBeds": 1,
-    "costPerNight": 497.64
-  }
+    "costPerNight": 294.56
+  },
+  {
+    "number": 22,
+    "roomType": "single room",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 350.31
+  },
+  {
+    "number": 23,
+    "roomType": "residential suite",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 2,
+    "costPerNight": 176.36
+  },
+  {
+    "number": 24,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 327.24
+  },
 ]
+
+export default roomData;
