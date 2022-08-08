e97119fc63df9b79435373c89c6ed5733a3cd718
Anthony Shellman <atshellman@gmail.com>
2022-08-08 16:12:30 -0600

    Add test file, relocate test data files

---
+++ test/Reservation-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import Reservation from '../src/classes/Reservation';
+import customerData from './customer-test-data';
+import roomData from './room-test-data';
+import bookingData from './booking-test-data';
+
+
+describe('Reservation', () => {
+
+
+})
